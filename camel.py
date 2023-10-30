7cb95dce220f777416db52a077821d5d3b72ff8b
test <test>
2023-10-30 11:03:53 +0700

    test

---
+++ camel.py
@@ -0,0 +1,17 @@
+
+camelCase=input("Верблюжий стиль: ")
+
+
+
+for l in camelCase:
+
+
+    if l.isupper():
+
+       print("_" + l.lower(), end="")
+
+
+    else:
+        print(l, end="")
+
+print()
